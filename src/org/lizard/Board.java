19c9bdba5729c178b2ca05cb731685bbb7a23f97
Collin Fairlie <collin.fairlie@gmail.com>
2021-02-01 20:37:01 -0800

    Completed all needed functionality to parse information from XML for rooms and create their instances where needed, adding exits and items to rooms where needed.

--- src/org/lizard/Board.java
+++ src/org/lizard/Board.java
@@ -14,7 +14,7 @@ public class Board {
     private Room currentRoom;
     private Map<String, Room> allRooms = new HashMap<>();
     private Map<String, Map<String, String>> allExits = new HashMap<>();
-    private Map<String, List<String>> allItems = new HashMap<>();
+    private Map<String, Item> allItems = new HashMap<>();
 
     public Board(){
         createMap();
@@ -29,7 +29,6 @@ public class Board {
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 // Create a way to hold the exits for each room before placing them in allExits
                 Map<String,String> exits = new HashMap<>();
-                List<String> items = new ArrayList<>();
                 Element eElement = (Element) node;
 
                 // Gain and save all relevant information needed to create a Room class object
@@ -49,19 +48,6 @@ public class Board {
                     exits.put(eElement.getElementsByTagName("path").item(itr2).getTextContent(), eElement.getElementsByTagName("pathName").item(itr2).getTextContent());
                 }
 
-                // Gain all items listed in this particular room and save them in all
-                for (int itr3 = 0; itr3 < eElement.getElementsByTagName("item").getLength(); itr3++) {
-                    String roomItem = eElement.getElementsByTagName("item").item(itr3).getTextContent();
-                    items.add(roomItem);
-        //            System.out.println(roomItem);
-                }
-
-                // Add the new items ArrayList with the corresponding room name to allItems in order to build the Room class object items later
-                // Only add if the room has an item
-                if (items.size() >= 1) {
-                    allItems.put(roomName, items);
-                }
-
                 // Add the new exits Map object with the corresponding room name to allExits in order to build the Room class object exits later
                 allExits.put(roomName, exits);
 
@@ -69,39 +55,14 @@ public class Board {
                 allRooms.put(roomName, room);
             }
         }
-        System.out.println(allItems);
+
+        // Add all exits to their respective rooms
+        addExitsToRooms();
         // Add all items to their respective rooms
         addItemsToRooms();
 
-        // Iterate over the nested Map objects in order to add all exits to their respective room
-        Iterator<Map.Entry<String, Map<String, String>>> iterator = allExits.entrySet().iterator();
-        while (iterator.hasNext()) {
-            // When calling entry.getKey(), you will get the String room name from allExits
-            // When calling entry.getValue(), you will get the Map<String, String> of the respective rooms exits
-            Map.Entry<String, Map<String, String>> entry = iterator.next();
-
-            // Create a second iterator for the inner Map
-            Iterator<Map.Entry<String, String>> iterator2 = entry.getValue().entrySet().iterator();
-            while (iterator2.hasNext()) {
-                // entry2.getKey() will return the String direction ("west", "north", etc)
-                // entry2.getValue() will return the String room ("library", "kitchen", etc)
-                Map.Entry<String, String> entry2 = iterator2.next();
-                // Get the Room class object from allRooms to create a new exit with the proper room exit
-                allRooms.get(entry.getKey()).createRoom(entry2.getKey(), allRooms.get(entry2.getValue()));
-            }
-        }
-
-
-        Item match = new Item("match");
-        Item candle = new Item("candle", new Lock(match, "The candle has lit. You run west!", new Command(2, new Direction("west"))));
-//        closet.addItemToRoom(match);
-//        living.addItemToRoom(candle);
-//        treasureRoom.addItemToRoom(new Item("key"));
-
-
-//        this.currentRoom = living;
+        // Set starting room.
         this.currentRoom = allRooms.get("living");
-
     }
 
     public Room getCurrentRoom() {
@@ -142,7 +103,28 @@ public class Board {
         return null;
     }
 
-//    private void addItemsToRooms (Element eElement, Room room) {
+
+    private void addExitsToRooms() {
+        // Iterate over the nested Map objects in order to add all exits to their respective room
+        Iterator<Map.Entry<String, Map<String, String>>> iterator = allExits.entrySet().iterator();
+        while (iterator.hasNext()) {
+            // When calling entry.getKey(), you will get the String room name from allExits
+            // When calling entry.getValue(), you will get the Map<String, String> of the respective rooms exits
+            Map.Entry<String, Map<String, String>> entry = iterator.next();
+
+            // Create a second iterator for the inner Map
+            Iterator<Map.Entry<String, String>> iterator2 = entry.getValue().entrySet().iterator();
+            while (iterator2.hasNext()) {
+                // entry2.getKey() will return the String direction ("west", "north", etc)
+                // entry2.getValue() will return the String room ("library", "kitchen", etc)
+                Map.Entry<String, String> entry2 = iterator2.next();
+                // Get the Room class object from allRooms to create a new exit with the proper room exit
+                allRooms.get(entry.getKey()).createRoom(entry2.getKey(), allRooms.get(entry2.getValue()));
+            }
+        }
+    }
+
+
     private void addItemsToRooms () {
 
         NodeList nodeList = XMLParser("xml/Items.xml", "item");
@@ -155,22 +137,30 @@ public class Board {
                 // Make the node an element to gain access to text content
                 Element itemElement = (Element) node;
 
-//                System.out.println(itemElement.getElementsByTagName("id").item(0).getTextContent());
-//                System.out.println(itemElement.getElementsByTagName("name").item(0).getTextContent());
-//                System.out.println(itemElement.getElementsByTagName("roomName").item(0).getTextContent());
-//                System.out.println(itemElement.getElementsByTagName("description").item(0).getTextContent());
                 // Save values of item
                 String itemId = itemElement.getElementsByTagName("id").item(0).getTextContent();
                 String itemName = itemElement.getElementsByTagName("name").item(0).getTextContent();
                 String roomName = itemElement.getElementsByTagName("roomName").item(0).getTextContent();
+                String lockKey = itemElement.getElementsByTagName("lockKey").item(0).getTextContent();
+                String lockMessage = itemElement.getElementsByTagName("lockMessage").item(0).getTextContent();
+                String commandInt = itemElement.getElementsByTagName("commandInt").item(0).getTextContent();
+                String commandDirection = itemElement.getElementsByTagName("commandDirection").item(0).getTextContent();
                 String itemDescription = itemElement.getElementsByTagName("description").item(0).getTextContent();
 
-                // Create new instance of the item
-                Item roomItem = new Item(itemName);
 
-                // Add newly created item to its respective room
-                allRooms.get(roomName).addItemToRoom(roomItem);
-//                System.out.println(allRooms.get(roomName).getItems());
+                // Create new instance of the item
+                if (lockKey.equals("none")) {
+                    Item roomItem = new Item(itemName);
+                    allItems.put(itemName, roomItem);
+                    // Add newly created item to its respective room
+                    allRooms.get(roomName).addItemToRoom(roomItem);
+                } else {
+                    System.out.println("Item Name: " + itemName + " Item: " + allItems.get(lockKey) + " Lock Message: " + lockMessage + " Command Int: " + commandInt + " Command Direction: " + commandDirection);
+                    Item roomItem = new Item(itemName, new Lock(allItems.get(lockKey), lockMessage, new Command(Integer.parseInt(commandInt), new Direction(commandDirection))));
+
+                    // Add newly created item to its respective room
+                    allRooms.get(roomName).addItemToRoom(roomItem);
+                }
             }
         }
     }
