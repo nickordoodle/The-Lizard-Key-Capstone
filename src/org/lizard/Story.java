b79f248effbb64c29e1a23312b08e0df83eca04c
Collin Fairlie <collin.fairlie@gmail.com>
2021-02-03 20:15:21 -0800

    Revised paragraph structure.

--- src/org/lizard/Story.java
+++ src/org/lizard/Story.java
@@ -2,13 +2,13 @@ package org.lizard;
 
 class Story {
     static String introduction() {
-        return "  Like a dream, your thoughts carry you to an unknown universe. Four bare walls keep you as a prisoner to an overly bright room with no windows. " +
+        return "Like a dream, your thoughts carry you to an unknown universe. Four bare walls keep you as a prisoner to an overly bright room with no windows. " +
                 "Shelves with millions of keys in varying sizes, shapes, and colors line the floors like passageways to doors around you.\n" +
-                "  What lies behind each door is unknown to you, but your options are limited, for this is no dream, and you are not imagining what you are seeing. " +
+                "What lies behind each door is unknown to you, but your options are limited, for this is no dream, and you are not imagining what you are seeing. " +
                 "The keys you touch are as real as the keys to your home. The floor you stand on is as hard as the surface beneath your bed.\n" +
-                "  Like many others before you, you have become a pet to a man named Rex Verwirrtheit, locksmith, creator of worlds, and master of confusion. " +
+                "Like many others before you, you have become a pet to a man named Rex Verwirrtheit, locksmith, creator of worlds, and master of confusion. " +
                 "With no two worlds being the same, Mister Verwirrtheit has created each maze uniquely odd and fantastically tumultuous for every mouse searching for its cheese.\n" +
-                "  It is up to you to find the way out of this prison, for if you do not, you will forever be in an endless loop of rooms that lead to other rooms," +
+                "It is up to you to find the way out of this prison, for if you do not, you will forever be in an endless loop of rooms that lead to other rooms," +
                 "the final and single exit a mystery never to be given away freely.";
 
     }
