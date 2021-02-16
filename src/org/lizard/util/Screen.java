6119c418aff9cc72062e3a8d52de41b9037ae40d
Nick Scherer <nscherer30@gmail.com>
2021-02-16 14:43:48 -0800

    Fixes AB#786 refactored background image to completely remove when Enter Game is pressed

--- src/org/lizard/util/Screen.java
+++ src/org/lizard/util/Screen.java
@@ -10,15 +10,27 @@ public class Screen {
     // Get the screen size's dimensions
     public static final Dimension SCREEN_DIMENSIONS = Toolkit.getDefaultToolkit().getScreenSize();
 
-    public static final int WIDTH = (int) SCREEN_DIMENSIONS.getWidth();
+    public static final int SCREEN_WIDTH = (int) SCREEN_DIMENSIONS.getWidth();
 
-    public static final int HEIGHT = (int) SCREEN_DIMENSIONS.getHeight();
+    public static final int SCREEN_HEIGHT = (int) SCREEN_DIMENSIONS.getHeight();
 
-    public static final int CENTER_HORIZONTAL = WIDTH / 2;
+    public static final int SPECIFIED_WIDTH = 1600;
 
-    public static final int CENTER_VERTICAL = HEIGHT / 2;
+    public static final int SPECIFIED_HEIGHT = 1060;
+
+    public static final int CENTER_HORIZONTAL = SPECIFIED_WIDTH / 2;
+
+    public static final int CENTER_VERTICAL = SPECIFIED_HEIGHT / 2;
 
     public static final int MAP_SQUARE_SIZE = 80;
+
+    public static int getLeftXCoordinateForElement(int elementWidth){
+        return (CENTER_HORIZONTAL - (elementWidth / 2));
+    }
+
+    public static int getTopYCoordinateForElement(int elementHeight){
+        return (CENTER_VERTICAL - (elementHeight / 2));
+    }
 
 
 }
